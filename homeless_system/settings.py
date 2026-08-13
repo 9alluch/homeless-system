c750ab33e5e6dae0e4fb6837ef4449555f7d90d0
9alluch <9aluch2@gmail.com>
2026-08-13 21:48:07 +0100

    Configure production settings

--- homeless_system/settings.py
+++ homeless_system/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-od5(czlxgp)=6)95e&lwnurheocxf7v^0824nug^41%x-abmr9'
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
